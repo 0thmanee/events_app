729c6513a13f3778dbf48d75c764c351ee6a71d2
Betox <othmanebouchtaa@gmail.com>
2025-05-31 12:25:42 +0100

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }]
-    ],
+    presets: ['babel-preset-expo'],
     plugins: [
-      // Required for expo-router
-      "expo-router/babel",
-      // Required for react-native-reanimated
-      "react-native-reanimated/plugin",
-      // NativeWind
-      "nativewind/babel",
+      'expo-router/babel',
+      'react-native-reanimated/plugin',
     ],
   };
 }; 
